aa563f426e9d1e5f79f533e319c7feb5e4dd797f
zhangchen <windyzj@163.com>
2020-05-21 12:04:23 +0800

    面向datav接口发布

--- src/main/java/com/atguigu/gmall1122/publisher/controller/DataVController.java
+++ src/main/java/com/atguigu/gmall1122/publisher/controller/DataVController.java
@@ -2,12 +2,15 @@ package com.atguigu.gmall1122.publisher.controller;
 
 
 import com.alibaba.fastjson.JSON;
+import com.atguigu.gmall1122.publisher.bean.UserInfo;
 import com.atguigu.gmall1122.publisher.service.MysqlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,9 +28,22 @@ public class DataVController {
          }
         startDate = startDate.replace("_", " ");
         endDate = endDate.replace("_", " ");
-        List<Map> trademardSum = mysqlService.getTrademardSum(startDate, endDate);
+        List<Map> trademarkSumList = mysqlService.getTrademarkSum(startDate, endDate);
 
-        return JSON.toJSONString(trademardSum) ;
+        //根据datav调整结构
+        List<Map>  datavVList=new ArrayList<>();
+        for (Map trademarkSumMap : trademarkSumList) {
+           Map dataVMap=  new HashMap();
+
+            dataVMap.put("x",trademarkSumMap.get("tm_name")) ;
+            dataVMap.put("y",trademarkSumMap.get("amount")) ;
+            dataVMap.put("s","1") ;
+            datavVList.add(dataVMap);
+        }
+        UserInfo userInfo = new UserInfo("1001","zhang3");
+
+
+        return JSON.toJSONString(datavVList) ;
 
 
     }
